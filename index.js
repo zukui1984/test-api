dae19b9f5fd45e5f3e9b31980dfa65078822e4e7
zukui <71074389+zukui1984@users.noreply.github.com>
2021-04-29 23:00:22 +0200

    install ejs and add home.ejs

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express= require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+
+// function handleIndexRequest(req, res) {
+//   res.send('Hello World!');
+// }
+
+function handleIndexRequest(req, res) {
+  const name = req.query.name || 'Stranger';
+  res.render('home', {name : name});
+}
+
+app.get('/', handleIndexRequest);
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+console.log('Example app listening on port 3000!');
+
+app.set('view engine', 'ejs');
 });
